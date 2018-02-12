1127e0396abebdda9532f42a91cbce5cda05b66f
Valeriy Garzha <valerasebafr1@gmail.com>
2018-02-12 11:23:59 +0200

    Local enviroment configuration

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -14,4 +14,17 @@ if (!isset($_ENV['PANTHEON_ENVIRONMENT'])) {
   );
 
   $settings['hash_salt'] = '5xAVyif6ZWvMWuegmfvCLp_m644IMHiltlJxTeEvaP0HupX3x3TlxPnhsVTY60MgAC09M4-upQ';
+
+  $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+
+  assert_options(ASSERT_ACTIVE, TRUE);
+  \Drupal\Component\Assertion\Handle::register();
+  /**
+   * Show all error messages, with backtrace information.
+   *
+   * In case the error level could not be fetched from the database, as for
+   * example the database connection failed, we rely only on this value.
+   */
+  $config['system.logging']['error_level'] = 'verbose';
+
 }
